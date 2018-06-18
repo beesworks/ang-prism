f43b3549cd80d608055fd544e9efc8675703fafc
Medhat Dawoud <medhatdawoud@gmail.com>
2018-06-18 11:44:37 +0200

    Some changes to the project

---
+++ src/app/ang-prism.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AngPrismComponent } from './ang-prism.component';
+
+@NgModule({
+  declarations: [AngPrismComponent],
+  imports: [],
+  exports: [AngPrismComponent],
+  providers: [],
+})
+export class AngPrismModule {}
